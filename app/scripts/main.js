65e8415422ca618a05f088db6b74b5fc3ee6c8cb
jasalguero <jasalguero@gmail.com>
2016-04-09 00:59:20 +0200

    converting js to module pattern

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,118 +1,187 @@
-const DATA_SOURCE_URL = "https://docs.google.com/spreadsheets/d/1hVa7vtHCc7WGkf0idxU0j5YWX0eX0jzavMR5GncG-nU/edit#gid=0";
+var RPB = ((APP) => {
 
-const DEFAULT = {
+  const CONSTANTS = {
+    SPREADSHEETS: {
+      GENERAL: "https://docs.google.com/spreadsheets/d/1hVa7vtHCc7WGkf0idxU0j5YWX0eX0jzavMR5GncG-nU/edit#gid=0",
+      MEDICAL: "https://docs.google.com/spreadsheets/d/1wjmRrkN9WVB4KIeKBy8wDDJ8E51Mh2-JxIBy2KNMFRQ/edit#gid=0",
+      JURIDICAL: "https://docs.google.com/spreadsheets/d/1D7jo-tAyQkmfYvVyT27nZ93ZkyFcZg2vEvf4OMbXJ_c/edit#gid=0"
+    },
+    LANGUAGES: {}
+  };
 
-};
+  let ELEMS = {
+    TABLE: null
+  };
 
-let CONFIG = {
-  LANGUAGES: {}
-};
+  const DEFAULTS = {
+    target: CONSTANTS.SPREADSHEETS.GENERAL,
+    languages: ['C', 'D', 'E', 'F'],
+    container: 'rpb-container'
+  };
 
-let getAvailableLanguages = () => {
-  var query = new google.visualization.Query(DATA_SOURCE_URL);
-  query.setQuery('select * LIMIT 1 OFFSET 1');
-  query.send(processAvailableLanguages);
-};
+  let config = {};
 
-let processAvailableLanguages = (response) => {
-  if (response.isError()) {
-    console.error('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-  } else {
-    var dataTable = response.getDataTable();
+  var APP = APP || {};
 
-    // get all the values except the first two columns that don't hold language name
-    for(let i = 2; i < dataTable.getNumberOfColumns(); i ++) {
-      CONFIG.LANGUAGES[dataTable.getColumnId(i)] = {id: dataTable.getColumnId(i), label: dataTable.getValue(0,i)};
+  let API = {};
+
+  APP.getAvailableLanguages = () => {
+    var query = new google.visualization.Query(CONSTANTS.SPREADSHEETS.GENERAL);
+    query.setQuery('select * LIMIT 1 OFFSET 1');
+    query.send(APP.processAvailableLanguages);
+  };
+
+  APP.processAvailableLanguages = (response) => {
+    if (response.isError()) {
+      console.error('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    } else {
+      var dataTable = response.getDataTable();
+
+      // get all the values except the first two columns that don't hold language name
+      for (let i = 2; i < dataTable.getNumberOfColumns(); i++) {
+        CONSTANTS.LANGUAGES[dataTable.getColumnId(i)] = {id: dataTable.getColumnId(i), label: dataTable.getValue(0, i)};
+      }
+      // APP.createLanguageForm(dataTable);
+      // APP.querySpreadsheet(config);
     }
-    createLanguageForm(dataTable);
-    querySpreadsheet();
-  }
-};
+  };
 
-let createLanguageForm = (dataTable) => {
-  // remove loading status
-  let elem = document.getElementById('langLoading');
-  elem.parentNode.removeChild(elem);
+  APP.createLanguageForm = (dataTable) => {
+    // remove loading status
+    let elem = document.getElementById('langLoading');
+    elem.parentNode.removeChild(elem);
 
-  let langForm = document.getElementById('langForm');
+    let langForm = document.getElementById('langForm');
 
-  for(let i = 2; i < dataTable.getNumberOfColumns(); i ++) {
-    let langLabel = dataTable.getValue(0,i);
-    let langId = dataTable.getColumnId(i);
+    for (let i = 2; i < dataTable.getNumberOfColumns(); i++) {
+      let langLabel = dataTable.getValue(0, i);
+      let langId = dataTable.getColumnId(i);
 
-    let wrapper = document.createElement("div");
-    let input = document.createElement("input");
-    let label = document.createElement("span");
+      let wrapper = document.createElement("div");
+      let input = document.createElement("input");
+      let label = document.createElement("span");
 
-    input.type = "checkbox";
-    input.value = langId;
-    label.innerHTML = langLabel;
+      input.type = "checkbox";
+      input.value = langId;
+      label.innerHTML = langLabel;
 
-    wrapper.appendChild(input);
-    wrapper.appendChild(label);
-    langForm.appendChild(wrapper);
-  }
-
-  let updateButton = document.createElement("input");
-  updateButton.onclick = updateQuery;
-  updateButton.type = "button";
-  updateButton.value = "Update Table";
-  updateButton.innerHTML = "Update Table";
-  langForm.appendChild(updateButton);
-};
-
-let updateQuery = () => {
-  let query = "SELECT B";
-  let  selectedLanguages = document.getElementById('langForm').querySelectorAll('input[type="checkbox"]:checked');
-  for (let i = 0; i < selectedLanguages.length; i++) {
-    query += ", " + selectedLanguages[i].value;
-  }
-
-  query += " OFFSET 4";
-
-  var theQuery = new google.visualization.Query(DATA_SOURCE_URL);
-  theQuery.setQuery(query);
-  console.log(query);
-  theQuery.send(handleQueryResponse);
-};
-
-
-
-
-let querySpreadsheet = (config) => {
-  var query = new google.visualization.Query(DATA_SOURCE_URL);
-  query.setQuery('SELECT B, C, D, E, F OFFSET 4');
-  query.send(handleQueryResponse);
-};
-
-let handleQueryResponse = (response) => {
-  if (response.isError()) {
-    console.error('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-  } else {
-
-    let dataTable = response.getDataTable();
-
-    // set the column names
-    for(let i = 0; i < dataTable.getNumberOfColumns(); i ++) {
-      let columnId = dataTable.getColumnId(i);
-      let language = CONFIG.LANGUAGES[columnId] ? CONFIG.LANGUAGES[columnId].label : "";
-      dataTable.setColumnLabel(i, language);
+      wrapper.appendChild(input);
+      wrapper.appendChild(label);
+      langForm.appendChild(wrapper);
     }
 
-    dataTable.setColumnLabel(0, 'PHRASE');
+    let updateButton = document.createElement("input");
+    updateButton.onclick = APP.updateQuery;
+    updateButton.type = "button";
+    updateButton.value = "Update Table";
+    updateButton.innerHTML = "Update Table";
+    langForm.appendChild(updateButton);
+  };
 
-    drawTable(new google.visualization.DataView(response.getDataTable()))
-  }
-};
+  APP.updateQuery = () => {
+    let query = "SELECT B";
+    let selectedLanguages = document.getElementById("langForm").querySelectorAll('input[type="checkbox"]:checked');
+    for (let i = 0; i < selectedLanguages.length; i++) {
+      query += ", " + selectedLanguages[i].value;
+    }
 
-let drawTable = (dataView) => {
-  var table = new google.visualization.Table(document.getElementById('output'));
+    query += " OFFSET 4";
 
+    var theQuery = new google.visualization.Query(CONSTANTS.SPREADSHEETS.GENERAL);
+    theQuery.setQuery(query);
+    theQuery.send(APP.handleQueryResponse);
+  };
 
-  table.draw(dataView);
-};
+  APP.querySpreadsheet = (config) => {
+    let queryObject = new google.visualization.Query(config.target);
+    let query = "SELECT B";
+    for (let i = 0; i < config.languages.length; i++) {
+      query += ", " + config.languages[i];
+    }
 
-window.onload = () => {
-  getAvailableLanguages();
-};
+    query += " OFFSET 4";
+
+    queryObject.setQuery(query);
+    queryObject.send(APP.handleQueryResponse);
+  };
+
+  APP.handleQueryResponse = (response) => {
+    if (response.isError()) {
+      console.error("Error in query: " + response.getMessage() + " " + response.getDetailedMessage());
+    } else {
+
+      let dataTable = response.getDataTable();
+
+      // set the column names
+      for (let i = 0; i < dataTable.getNumberOfColumns(); i++) {
+        let columnId = dataTable.getColumnId(i);
+        let language = CONSTANTS.LANGUAGES[columnId] ? CONSTANTS.LANGUAGES[columnId].label : "";
+        dataTable.setColumnLabel(i, language);
+      }
+
+      dataTable.setColumnLabel(0, "PHRASE");
+
+      APP.drawTable(new google.visualization.DataView(response.getDataTable()))
+    }
+  };
+
+  APP.drawTable = (dataView) => {
+    var table = new google.visualization.Table(ELEMS.TABLE);
+    table.draw(dataView);
+  };
+
+  /**
+   * Create the DOM structure for the app
+   */
+  APP.createDOM = (config) => {
+    ELEMS.CONTAINER = document.getElementsByClassName(config.container)[0];
+
+    APP.createSettingsButton();
+    APP.createSettings();
+    APP.createTableElems();
+  };
+
+  APP.createSettings = () => {
+    ELEMS.SETTINGS = document.createElement("div");
+    ELEMS.SETTINGS.classList.add("rpb-settings-container");
+    ELEMS.CONTAINER.appendChild(ELEMS.SETTINGS);
+  };
+
+  APP.createSettingsButton = () => {
+    let settingsButton = document.createElement("input");
+    settingsButton.onclick = APP.showSettings;
+    settingsButton.type = "button";
+    settingsButton.value = "Settings";
+    settingsButton.classList.add("btn");
+    settingsButton.classList.add("rpb-settings-btn");
+    ELEMS.CONTAINER.appendChild(settingsButton);
+  };
+
+  APP.showSettings = () => {
+    ELEMS.TABLE.classList.toggle("hidden");
+    ELEMS.SETTINGS.classList.toggle("hidden");
+  };
+
+  APP.createTableElems = () => {
+    ELEMS.TABLE = document.createElement("div");
+    ELEMS.TABLE.classList.add("rpb-table");
+    ELEMS.CONTAINER.appendChild(ELEMS.TABLE);
+  };
+
+  /**
+   * Starting point for the module
+   */
+  APP.initialize = () => {
+    // get provided defaults or the general defaults
+    APP.config = APP.CONFIG || DEFAULTS;
+
+    APP.createDOM(APP.config);
+    APP.getAvailableLanguages();
+    APP.querySpreadsheet(APP.config);
+  };
+
+  window.onload = () => {
+    APP.initialize();
+  };
+})(RPB || {});
 
